eb8be41d050844a9d7a32118ea37b05597c285c7
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-12-19 15:41:10 +0100

    Header Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Client</div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
