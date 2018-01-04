ddd0a1ea533c6c1ebb653bd2ab7f2fa038cad10d
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2018-01-04 13:14:35 +0100

    signup component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                U do signup
+            </div>
+        );
+    }
+}
+
+export default Signup;
