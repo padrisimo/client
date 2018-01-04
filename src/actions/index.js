43742d323c342b17c53f0392b58fad9cf9fc3bac
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2018-01-04 12:44:03 +0100

    signout action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import { 
     AUTH_USER,
-    AUTH_ERROR
+    AUTH_ERROR,
+    UNAUTH_USER
  } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
@@ -30,4 +31,9 @@ export function authError(error) {
         type: AUTH_ERROR,
         payload: error
     }
+}
+
+export function signoutUser() {
+    localStorage.removeItem('token');
+    return { type: UNAUTH_USER };
 }
