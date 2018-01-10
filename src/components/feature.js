5d9077763a52d79fec5ab826a4c19f961ab29564
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2018-01-10 18:14:09 +0100

    securing individual routes with hoc

---
+++ src/components/feature.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Feature extends Component {
+    render() {
+        return (
+            <div>
+                ou yea thats the feature!
+            </div>
+        );
+    }
+}
+
+export default Feature;
