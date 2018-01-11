2674ce000ee6ba17bb81e87f78c867c90b75689c
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2018-01-11 15:23:32 +0100

    auto authenticating users

--- src/index.js
+++ src/index.js
@@ -14,11 +14,18 @@ import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
 import Welcome from './components/welcome';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk, logger)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: AUTH_USER})
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
       <Router history={browserHistory}>
         <Route path='/' component={App}>
           <IndexRoute component={Welcome} />
